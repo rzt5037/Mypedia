7736462e98d7d02c024c70fa3a5cb27e49c17973
Richard Travis <r.z.travis@gmail.com>
2018-01-21 14:40:33 -0700

    Markdown functionality added for wiki bodies

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -4,4 +4,25 @@ module WikisHelper
       (wiki.private == true && current_user.id == wiki.user_id) || (wiki.private == false) || (current_user.admin?)
     end
   end
+
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
